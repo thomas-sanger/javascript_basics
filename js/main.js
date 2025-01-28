165c068b7d72e8ff32dab2d8f535f9b68488cb39
Thomas Sanger <thomas.sanger92@gmail.com>
2025-01-27 20:02:27 -0700

    Learned conditional statements, as well as the ternary operator.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,49 @@
 // variabl;es in JavaScript
 // these variables are set globally
 
-let x = "Hello"; // string value
-let y = "World!"; // string value
+// let x = "Hello"; // string value
+// let y = "World!"; // string value
 
-const blnSwitch = true; // constants are set in stone and cannot be changed
+// const blnSwitch = true; // constants are set in stone and cannot be changed
 
-test1(); // function call
+// test1(); // function call
 
-function test1() {
-  let z = 12.2; // local in the function
-  let y = 5; // local in the function
-  test2(z, y);
-}
+// function test1() {
+//   let z = 12.2; // local in the function
+//   let y = 5; // local in the function
+//   test2(z, y);
+// }
 
-function test2(z, y) {
-  // define a parameter
-  console.log(z + y);
-}
+// function test2(z, y) {
+//   // define a parameter
+//   console.log(z + y);
+// }
+
+// conditional statements are a comparison of values
+
+let myName = "Jen";
+
+// if (myName.toLowerCase == "bob") {
+//   console.log("Equal");
+// } else if (myName.toLowerCase() == "nick") {
+//   console.log("Professor");
+// } else {
+//   console.log("Not equal");
+// }
+
+// switch (myName.toLowerCase()) {
+//   case "bob":
+//     console.log("Hey Bob");
+//     break; // this ends the switch statement
+//   case "nick":
+//     console.log("Hey Professor");
+//     break;
+//   case "jen":
+//     console.log("Hey Jen");
+//     break;
+//   default:
+//     console.log("Who are you?");
+// }
+
+// ternary
+console.log(myName == "Jenny" ? "Hey Jen" : "Who are you?");
